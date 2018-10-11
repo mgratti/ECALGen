d75acb312900e4ff39293af9c4131dde7d2cf769
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2018-10-11 09:49:54 +0200

    pile option added

--- private_production/gen_sim_digi/ecalGenDigi.sh
+++ private_production/gen_sim_digi/ecalGenDigi.sh
@@ -13,10 +13,10 @@ NEVENTS=50000
 # step 1: gen + sim
 echo 'Going to run step 1'
 
-cmsDriver.py DoublePhoton.py --conditions $CONDITIONS --geometry $GEOMETRY --beamspot Realistic25ns13TeVEarly2017Collision  --era $ERA --step GEN,SIM --datatier GEN-SIM --eventcontent FEVTDEBUG --fileout file:EGM-RunIISpring18_GEN_SIM.root -n $NEVENTS --no_exec 
+cmsDriver.py DoublePhoton.py --conditions $CONDITIONS --geometry $GEOMETRY --beamspot Realistic25ns13TeVEarly2017Collision  --era $ERA --pileup $PU --step GEN,SIM --datatier GEN-SIM --eventcontent FEVTDEBUG --fileout file:EGM-RunIISpring18_GEN_SIM.root -n $NEVENTS --no_exec 
 
 cmsRun DoublePhoton_py_GEN_SIM.py &> log_GEN_SIM
 
 # step 2: digi
 echo 'Going to run step 2'
-cmsDriver.py step2  --conditions $CONDITIONS --geometry $GEOMETRY  --beamspot Realistic25ns13TeVEarly2017Collision --era $ERA --step DIGI:pdigi_valid,L1,DIGI2RAW,HLT:@relval2018 --datatier GEN-SIM-DIGI-RAW  --eventcontent FEVTDEBUGHLT --filein file:EGM-RunIISpring18_GEN_SIM.root  --fileout file:EGM-RunIISpring18_GEN_SIM_DIGI.root -n $NEVENTS  &> log_DIGI
+cmsDriver.py step2  --conditions $CONDITIONS --geometry $GEOMETRY  --beamspot Realistic25ns13TeVEarly2017Collision --era $ERA --pileup $PU --step DIGI:pdigi_valid,L1,DIGI2RAW,HLT:@relval2018 --datatier GEN-SIM-DIGI-RAW  --eventcontent FEVTDEBUGHLT --filein file:EGM-RunIISpring18_GEN_SIM.root  --fileout file:EGM-RunIISpring18_GEN_SIM_DIGI.root -n $NEVENTS  &> log_DIGI
