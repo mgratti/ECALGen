93c9769f1539613b6658231c3ff567c7a2aa71ba
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-04-02 13:23:51 +0200

    fixed cutstomization of global tags

--- private_production/gen_sim_digi_reco/tags_to_override.py
+++ private_production/gen_sim_digi_reco/tags_to_override.py
@@ -1,3 +1,6 @@
+# Overriding tags og a global tag
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions#Customization_of_Global_Tags
+
 # First set is 180/fb
 
 ### MG: override several tags to new noise conditions
@@ -5,28 +8,18 @@ process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalLaserAPDPNRatiosRcd'),
            tag = cms.string('EcalLaserAPDPNRatios_TL180fb_v1_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalPedestalsRcd'),
            tag = cms.string('EcalPedestals_TL180fb_v1_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalSRSettingsRcd'),
            tag = cms.string('EcalSRSettings_TL180fb_v1_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalIntercalibConstantsMCRcd'),
            tag = cms.string('EcalIntercalibConstantsMC_TL180fb_v1_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalTPGLinearizationConstRcd'),
            tag = cms.string('EcalTPGLinearizationConst_TL180fb_v1_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalTPGPedestalsRcd'),
            tag = cms.string('EcalTPGPedestals_TL180fb_v1_mc'),
            ),
@@ -41,28 +34,18 @@ process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalLaserAPDPNRatiosRcd'),
            tag = cms.string('EcalLaserAPDPNRatios_TL450fb_v1cor_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalPedestalsRcd'),
            tag = cms.string('EcalPedestals_TL450fb_v1_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalSRSettingsRcd'),
            tag = cms.string('EcalSRSettings_TL450fb_v1_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalIntercalibConstantsMCRcd'),
            tag = cms.string('EcalIntercalibConstantsMC_TL450fb_v1_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalTPGLinearizationConstRcd'),
            tag = cms.string('EcalTPGLinearizationConst_TL450fb_v1cor_mc'),
            ),
-)
-process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalTPGPedestalsRcd'),
            tag = cms.string('EcalTPGPedestals_TL450fb_v1cor_mc'),
            ),
