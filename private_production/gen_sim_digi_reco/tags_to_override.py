76f53453c6093f983a8151a47a27cc97ea707924
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-06-11 13:53:29 +0200

    latest tags for neutrino gun study

--- private_production/gen_sim_digi_reco/tags_to_override.py
+++ private_production/gen_sim_digi_reco/tags_to_override.py
@@ -9,7 +9,6 @@ process.GlobalTag.toGet = cms.VPSet(
            tag = cms.string('EcalLaserAPDPNRatios_TL180fb_v1_mc'),
            ),
   cms.PSet(record = cms.string('EcalPedestalsRcd'),
-           #tag = cms.string('EcalPedestals_TL180fb_v1_mc'), # taken from data
            tag = cms.string('EcalPedestals_TL180fb_Model190fb_mc'), # obtained from model, like 450/fb
            ),
   #cms.PSet(record = cms.string('EcalSRSettingsRcd'),
@@ -19,6 +18,9 @@ process.GlobalTag.toGet = cms.VPSet(
            tag = cms.string('EcalSRSettings_fullreadout_v01_mc'),
            connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
            ),
+#  cms.PSet(record = cms.string('EcalIntercalibConstantsRcd'),
+#           tag = cms.string('EcalIntercalibConstants_TL180fb_v1_mc'),
+#           ),
   cms.PSet(record = cms.string('EcalIntercalibConstantsMCRcd'),
            tag = cms.string('EcalIntercalibConstantsMC_TL180fb_v1_mc'),
            ),
@@ -29,8 +31,14 @@ process.GlobalTag.toGet = cms.VPSet(
            tag = cms.string('EcalTPGPedestals_TL180fb_v1_mc'),
            ),
   cms.PSet(record = cms.string('EcalPFRecHitThresholdsRcd'),
-           #tag = cms.string('EcalPFRecHitThresholds_2018_def_mc'),
+           #tag = cms.string('EcalPFRecHitThresholds_2018_def_mc'), # 80/300 MeV
            tag = cms.string('EcalPFRecHitThresholds_UL_2018_2e3sig'),
+           ),
+  cms.PSet(record = cms.string('EcalADCToGeVConstantRcd'),
+           tag = cms.string('EcalADCToGeVConstant_2010_V2_Bon_mc'),
+           ),
+  cms.PSet(record = cms.string('EcalLaserAlphasRcd'),
+           tag = cms.string('EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1'),
            ),
 )
 ### end override
@@ -46,7 +54,7 @@ process.GlobalTag.toGet = cms.VPSet(
            tag = cms.string('EcalPedestals_TL450fb_v1_mc'),
            ),
  # cms.PSet(record = cms.string('EcalSRSettingsRcd'),
- #          tag = cms.string('EcalSRSettings_TL450fb_v1_mc'),
+ #          tag = cms.string('EcalSRSettings_TL450fb_v2_mc'),
  #          ),
   cms.PSet(record = cms.string("EcalSRSettingsRcd"),
            tag = cms.string('EcalSRSettings_fullreadout_v01_mc'),
@@ -55,6 +63,9 @@ process.GlobalTag.toGet = cms.VPSet(
   cms.PSet(record = cms.string('EcalIntercalibConstantsMCRcd'),
            tag = cms.string('EcalIntercalibConstantsMC_TL450fb_v1_mc'),
            ),
+#  cms.PSet(record = cms.string('EcalIntercalibConstantsRcd'),
+#           tag = cms.string('EcalIntercalibConstants_TL450fb_v1_mc'),
+#           ),
   cms.PSet(record = cms.string('EcalTPGLinearizationConstRcd'),
            tag = cms.string('EcalTPGLinearizationConst_TL450fb_v1cor_mc'),
            ),
@@ -65,6 +76,56 @@ process.GlobalTag.toGet = cms.VPSet(
            #tag = cms.string('EcalPFRecHitThresholds_2018_def_mc'), # 80/300 MeV
            tag = cms.string('EcalPFRecHitThresholds_TL450_mixed'),
            ),
+  cms.PSet(record = cms.string('EcalADCToGeVConstantRcd'),
+           tag = cms.string('EcalADCToGeVConstant_2010_V2_Bon_mc'), # std for MC
+           ),
+  cms.PSet(record = cms.string('EcalLaserAlphasRcd'),
+           tag = cms.string('EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1'), # alpha = 1
+           ),
 )
 ### end override
+
+# Third set is 550/fb
+
+### MG: override several tags to new noise conditions
+process.GlobalTag.toGet = cms.VPSet(
+  cms.PSet(record = cms.string('EcalLaserAPDPNRatiosRcd'),
+           tag = cms.string('EcalLaserAPDPNRatios_TL550fb_v1_mc'),
+           ),
+  cms.PSet(record = cms.string('EcalPedestalsRcd'),
+           tag = cms.string('EcalPedestals_TL550fb_v1_mc'),
+           ),
+ # cms.PSet(record = cms.string('EcalSRSettingsRcd'),
+ #          tag = cms.string('EcalSRSettings_TL550fb_v2_mc'),
+ #          ),
+  cms.PSet(record = cms.string("EcalSRSettingsRcd"),
+           tag = cms.string('EcalSRSettings_fullreadout_v01_mc'),
+           connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
+           ), 
+#  cms.PSet(record = cms.string('EcalIntercalibConstantsRcd'),
+#           tag = cms.string('EcalIntercalibConstants_TL550fb_v1_mc'),
+#           ),
+  cms.PSet(record = cms.string('EcalIntercalibConstantsMCRcd'),
+           tag = cms.string('EcalIntercalibConstantsMC_TL550fb_v1_mc'),
+           ),
+  cms.PSet(record = cms.string('EcalTPGLinearizationConstRcd'),
+           tag = cms.string('EcalTPGLinearizationConst_TL550fb_mc'),
+           ),
+  cms.PSet(record = cms.string('EcalTPGPedestalsRcd'),
+           tag = cms.string('EcalTPGPedestals_TL550fb_mc'),
+           ),
+  cms.PSet(record = cms.string('EcalPFRecHitThresholdsRcd'),
+           #tag = cms.string('EcalPFRecHitThresholds_2018_def_mc'), # 80/300 MeV
+           tag = cms.string('EcalPFRecHitThresholds_TL550_mixed'),
+           ),
+  cms.PSet(record = cms.string('EcalADCToGeVConstantRcd'),
+           tag = cms.string('EcalADCToGeVConstant_2010_V2_Bon_mc'), # std for MC
+           ),
+  cms.PSet(record = cms.string('EcalLaserAlphasRcd'),
+           tag = cms.string('EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1'), # alpha = 1
+           ),
+)
+### end override
+
+
 
