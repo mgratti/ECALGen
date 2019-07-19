f867d34ad4e65220d174e6c52786fe4febb8787c
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-07-19 11:11:21 +0200

    latest parameters for double photon close ecal

--- cmssw_changed_files/DoublePhoton_closeECAL.py
+++ cmssw_changed_files/DoublePhoton_closeECAL.py
@@ -4,28 +4,29 @@
 import FWCore.ParameterSet.Config as cms
 
 
-
-generator = cms.EDProducer("CloseByParticleGunProducer",
-     PGunParameters = cms.PSet(PartID = cms.vint32(22, 22),
-       NParticles = cms.int32(2),
-       EnMin = cms.double(1),
-       EnMax = cms.double(100.),
-       RMin = cms.double(0.),
-       RMax = cms.double(300.),
-       ZMin = cms.double(0.),
-       ZMax = cms.double(200.),
-       Delta = cms.double(100),
-       Pointing = cms.bool(True),
-       Overlapping = cms.bool(False),
-       RandomShoot = cms.bool(False),
-       MaxEta = cms.double(3.),
-       MaxPhi = cms.double(3.14159265359),
-       MinEta = cms.double(-3.),
-       MinPhi = cms.double(-3.14159265359),
-     ),
-     Verbosity = cms.untracked.int32(10),
-     psethack = cms.string('two close by particles'),
-     AddAntiParticle = cms.bool(False),
-     firstRun = cms.untracked.uint32(1)
+process.generator = cms.EDProducer("CloseByParticleGunProducer",
+                                     PGunParameters = cms.PSet(PartID = cms.vint32(22, 22),
+                                       NParticles = cms.int32(2),
+                                       EnMin = cms.double(1.),   # in GeV
+                                       EnMax = cms.double(100.),
+                                       RMin = cms.double(123.8), # in cm
+                                       RMax = cms.double(123.8),
+                                       ZMin = cms.double(-304.5),    # in cm
+                                       ZMax = cms.double(304.5),
+                                       Delta = cms.double(300),  # in cm  -> phi1-phi2 = Delta/R 
+                                       Pointing = cms.bool(True),# otherwise showers parallel/perpendicular to beam axis
+                                       Overlapping = cms.bool(False),
+                                       RandomShoot = cms.bool(False),
+                                       MaxPhi = cms.double(3.14159265359),
+                                       MinPhi = cms.double(-3.14159265359),
+                                       MaxEta = cms.double(0.), # dummy, it is not used
+                                       MinEta = cms.double(0.), # dummy, it is not used
+                                       ),
+                                     Verbosity = cms.untracked.int32(1),
+                                     psethack = cms.string('two particles close to EB'),
+                                     AddAntiParticle = cms.bool(False),
+                                     firstRun = cms.untracked.uint32(1)
 )
+
+
                                                                                                                                                             
